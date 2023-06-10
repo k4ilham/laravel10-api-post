7b2a02cc66b7ec0ffcd58df7fd45be354fdfd87c
Ilham Maulana <k4ilham@gmail.com>
2023-06-11 01:31:40 +0700

    delete data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -135,4 +135,26 @@ class PostController extends Controller
         //return response
         return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
     }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function destroy($id)
+    {
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete image
+        Storage::delete('public/posts/'.basename($post->image));
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
+    }
 }
