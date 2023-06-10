123a201eff0bedecb75eb6e6cf232abc28f38a79
Ilham Maulana <k4ilham@gmail.com>
2023-06-11 01:27:23 +0700

    insert data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,6 +11,9 @@ use App\Http\Controllers\Controller;
 //import Resource "PostResource"
 use App\Http\Resources\PostResource;
 
+//import Facade "Validator"
+use Illuminate\Support\Facades\Validator;
+
 class PostController extends Controller
 {
     /**
@@ -25,5 +28,40 @@ class PostController extends Controller
 
         //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
