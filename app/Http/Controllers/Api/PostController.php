219c23a9b64ed9ecfe404dd17a21fc85f54a9ff1
Ilham Maulana <k4ilham@gmail.com>
2023-06-11 01:30:25 +0700

    update data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,6 +11,9 @@ use App\Http\Controllers\Controller;
 //import Resource "PostResource"
 use App\Http\Resources\PostResource;
 
+//import Facade "Storage"
+use Illuminate\Support\Facades\Storage;
+
 //import Facade "Validator"
 use Illuminate\Support\Facades\Validator;
 
@@ -78,5 +81,58 @@ class PostController extends Controller
 
         //return single post as a resource
         return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.basename($post->image));
+
+            //update post with new image
+            $post->update([
+                'image'     => $image->hashName(),
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+
+        } else {
+
+            //update post without image
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
     }
 }
